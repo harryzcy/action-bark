79689be48a3ac149b14f4c5c88f4966030c562b4
Chongyi Zheng <git@zcy.dev>
2024-06-25 23:43:08 -0400

    Support optional parameters

--- src/client.ts
+++ src/client.ts
@@ -6,6 +6,14 @@ export interface RequestInput {
   body: string
   device_key: string
   level: string
+  badge: string | null
+  automatically_copy: string | null
+  copy: string | null
+  sound: string | null
+  icon: string | null
+  group: string | null
+  is_archive: string | null
+  url: string | null
   github_runs_url: string
 }
 
@@ -28,7 +36,14 @@ export async function request(input: RequestInput): Promise<RequestResponse> {
     device_key: input.device_key,
     category: 'category',
     level: input.level,
-    url: input.github_runs_url
+    badge: input.badge || null,
+    automatically_copy: input.automatically_copy || null,
+    copy: input.copy || null,
+    sound: input.sound || null,
+    icon: input.icon || null,
+    group: input.group || null,
+    is_archive: input.is_archive || null,
+    url: input.url || input.github_runs_url
   })
   return res.data as RequestResponse
 }
